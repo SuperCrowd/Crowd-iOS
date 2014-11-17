ace0fee147dc347a2e383697bdd5fe15455653fc
Bobby Gill <bobby.gill@gmail.com>
2014-11-17 15:21:50 -0500

    project settings update

---
+++ Build/Intermediates/Crowd.build/Debug-iphonesimulator/Crowd.build/Objects-normal/i386/AFURLSessionManager.d
@@ -0,0 +1,7 @@
+dependencies: \
+  /Users/bobby/Documents/dev/BlueLabelLabs-BL074-Crowd-iOS/Crowd/ALL\ CLASSES/AFNetworking/AFURLSessionManager.m \
+  /Users/bobby/Documents/dev/BlueLabelLabs-BL074-Crowd-iOS/Crowd/ALL\ CLASSES/AFNetworking/AFURLSessionManager.h \
+  /Users/bobby/Documents/dev/BlueLabelLabs-BL074-Crowd-iOS/Crowd/ALL\ CLASSES/AFNetworking/AFURLResponseSerialization.h \
+  /Users/bobby/Documents/dev/BlueLabelLabs-BL074-Crowd-iOS/Crowd/ALL\ CLASSES/AFNetworking/AFURLRequestSerialization.h \
+  /Users/bobby/Documents/dev/BlueLabelLabs-BL074-Crowd-iOS/Crowd/ALL\ CLASSES/AFNetworking/AFSecurityPolicy.h \
+  /Users/bobby/Documents/dev/BlueLabelLabs-BL074-Crowd-iOS/Crowd/ALL\ CLASSES/AFNetworking/AFNetworkReachabilityManager.h
---
+++ Build/Intermediates/Crowd.build/Debug-iphonesimulator/Crowd.build/Objects-normal/i386/SDWebImageDownloader.d
@@ -0,0 +1,6 @@
+dependencies: \
+  /Users/bobby/Documents/dev/BlueLabelLabs-BL074-Crowd-iOS/Crowd/ALL\ CLASSES/SDWebImage/SDWebImageDownloader.m \
+  /Users/bobby/Documents/dev/BlueLabelLabs-BL074-Crowd-iOS/Crowd/ALL\ CLASSES/SDWebImage/SDWebImageDownloader.h \
+  /Users/bobby/Documents/dev/BlueLabelLabs-BL074-Crowd-iOS/Crowd/ALL\ CLASSES/SDWebImage/SDWebImageCompat.h \
+  /Users/bobby/Documents/dev/BlueLabelLabs-BL074-Crowd-iOS/Crowd/ALL\ CLASSES/SDWebImage/SDWebImageOperation.h \
+  /Users/bobby/Documents/dev/BlueLabelLabs-BL074-Crowd-iOS/Crowd/ALL\ CLASSES/SDWebImage/SDWebImageDownloaderOperation.h
