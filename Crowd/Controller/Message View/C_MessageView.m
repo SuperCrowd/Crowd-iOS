f452faa4118f05f67a4f7fe08ede3b0fdf8e171b
Bobby Gill <bobby.gill@gmail.com>
2014-12-11 16:25:09 -0500

    final twilio changes

--- Crowd/Controller/Message View/C_MessageView.m
+++ Crowd/Controller/Message View/C_MessageView.m
@@ -272,7 +272,7 @@
             //we also inform the TwilioClient to mark the user as unavailable for call
             [[C_TwilioClient sharedInstance]setCallAvailbilityForClient:_message_UserInfo.SenderID isAvailable:NO];
         }
-        self.secondsToWaitToCheckAvailability = [setAvailableForCallResult objectForKey:@"RenewAfterSeconds"];
+        self.secondsToWaitToCheckAvailability = [NSNumber numberWithInt: CALL_AVAILABILITY_HEARTBEAT_INTERVAL];
         
         
         //now we schedule the timer to renew in the specified number of seconds
